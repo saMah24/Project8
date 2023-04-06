36bc4c69ad215b44c481c67bc8c3294c8155fe3b
mshah2class-qaengineering <mshah2class-qaengineering@localhost>
2023-04-06 14:07:21 -0400

    Initial Commit

---
+++ src/demo8/Test8.java
@@ -0,0 +1,13 @@
+package demo8;
+
+public class Test8 {
+	
+public static void main(String[] args) {
+		
+		System.out.println("GitHub HW8...");
+		
+		System.out.println("Testing Continues8...");
+		
+	}
+
+}
